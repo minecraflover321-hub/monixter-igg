f6f407da250f67e76f7d169d034a5791e914bfe6
minecraflover321-hub <minecraflover321@gmail.com>
2026-02-22 11:38:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,17 @@
-import os
-import threading
-from flask import Flask
+    import os
 from telegram.ext import Updater, CommandHandler
 
-# =====================
-# ENV
-# =====================
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN missing!")
 
-# =====================
-# FLASK
-# =====================
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Bot Running ✅"
-
-# =====================
-# TELEGRAM
-# =====================
 def start(update, context):
     update.message.reply_text(
         "🚀 Instagram Monitor Bot Active!\n\nPowered by @proxyfxc"
     )
 
-def run_bot():
+def main():
     updater = Updater(BOT_TOKEN, use_context=True)
     dp = updater.dispatcher
 
@@ -38,12 +21,5 @@ def run_bot():
     updater.start_polling()
     updater.idle()
 
-# =====================
-# MAIN
-# =====================
 if __name__ == "__main__":
-    t = threading.Thread(target=run_bot)
-    t.start()
-
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    main()
